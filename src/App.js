b57b35d09db4675215aa1d5fff430e0344995f0c
Hasan Talukder <mahadihasansiam2@gmail.com>
2023-10-12 20:19:44 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"; 14.1k {gzipped: 5.5k}
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+
+//14.1k (gzipped: 5.5k)
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -8,23 +10,22 @@ import SingleProduct from "./components/SingleProduct/SingleProduct";
 import Newsletter from "./components/Footer/Newsletter/Newsletter";
 import AppContext from "./utils/context";
 
-
 function App() {
     return (
-        <div>
-            <BrowserRouter>
-            <Header/>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/category/:id" element={<Category />} />
-                    <Route path="/product/:id" element={<SingleProduct />} />
-                </Routes>
-                <Newsletter/>
-                <Footer/>
-            </BrowserRouter>
-        </div>
+        <BrowserRouter>
+        <AppContext>
+
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/category/:id" element={<Category />} />
+                <Route path="/product/:id" element={<SingleProduct />} />
+            </Routes>
+            <Newsletter />
+            <Footer />
+        ß</AppContext>
+        </BrowserRouter>
     );
 }
-
 
 export default App;
